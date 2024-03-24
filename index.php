00ea70ed7719f165696646c3b0af676399ead17e
Giammaria Biffi <102854627+beef-e@users.noreply.github.com>
2024-03-24 18:46:52 +0100

    Adedd session start in php files

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once 'vendor/autoload.php';
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
--- login_controller.php
+++ login_controller.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once 'vendor/autoload.php';
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
@@ -6,3 +7,8 @@ $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader);
 
 echo $twig->render('login.html');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_SESSION['name'] = $_POST['name'];
+    $_SESSION['password'] = $_POST['password'];
+}
