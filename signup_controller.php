ad30b694824a6f506ee822ec2a80c7dbc56d6630
Giammaria Biffi <102854627+beef-e@users.noreply.github.com>
2024-03-28 15:29:30 +0100

    Update signup functionalities

--- signup_controller.php
+++ signup_controller.php
@@ -6,4 +6,29 @@ $loader = new \Twig\Loader\FilesystemLoader('templates');
 
 $twig = new \Twig\Environment($loader);
 
-echo $twig->render('signup.html');
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $phone = $_POST['phone'];
+    $identita = $_POST['id_number'];
+    $scadenza_id = $_POST['id_expiry'];
+    $birthdate = $_POST['birthdate'];
+    $credit_card = $_POST['credit_card'];
+
+    if (isset($_POST['license_number'])) {
+        $license = $_POST['license_number'];
+        $license_expiry = $_POST['license_expiry'];
+    }
+
+    // echo $name . $surname . $username . $email . $password . $phone . $identita . $scadenza_id . $birthdate . $credit_card . $license . $license_expiry;
+    // TODO check if the use already exists
+
+    // TODO insert the user into the database
+
+    // TODO reload to the page with success message
+} else {
+    echo $twig->render('signup.html');
+}
