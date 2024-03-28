e53a9b9a867f4f456f3f82ef31d2f5e238c57add
Giammaria Biffi <102854627+beef-e@users.noreply.github.com>
2024-03-28 16:23:10 +0100

    Add create trip page

---
+++ createtrip_controller.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('create_trip.html');
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $partenza = $_POST['departure'];
+    $destinazione = $_POST['destination'];
+    $data = $_POST['departure_date'];
+    $ora = $_POST['departure_time'];
+    $posti = $_POST['seats'];
+    $prezzo = $_POST['price'];
+}
