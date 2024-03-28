63754bb11e4b75812372d995d56a10e914f83537
Giammaria Biffi <102854627+beef-e@users.noreply.github.com>
2024-03-28 16:37:59 +0100

    Add detailed trip page

---
+++ trip_controller.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('trip.html');
