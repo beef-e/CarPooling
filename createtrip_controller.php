c08e509ee4e02371c45ee6bbeccee49cbf1c945e
Giammaria Biffi <102854627+beef-e@users.noreply.github.com>
2024-03-30 20:04:18 +0100

    Added user role validation

--- createtrip_controller.php
+++ createtrip_controller.php
@@ -6,13 +6,17 @@ $loader = new \Twig\Loader\FilesystemLoader('templates');
 
 $twig = new \Twig\Environment($loader);
 
-echo $twig->render('create_trip.html');
+if ($_SESSION['role'] != 'd') {
+    header('Location: home_controller.php');
+} else {
+    echo $twig->render('create_trip.html');
 
-if ($_SERVER['REQUEST_METHOD'] === "POST") {
-    $partenza = $_POST['departure'];
-    $destinazione = $_POST['destination'];
-    $data = $_POST['departure_date'];
-    $ora = $_POST['departure_time'];
-    $posti = $_POST['seats'];
-    $prezzo = $_POST['price'];
+    if ($_SERVER['REQUEST_METHOD'] === "POST") {
+        $partenza = $_POST['departure'];
+        $destinazione = $_POST['destination'];
+        $data = $_POST['departure_date'];
+        $ora = $_POST['departure_time'];
+        $posti = $_POST['seats'];
+        $prezzo = $_POST['price'];
+    }
 }
