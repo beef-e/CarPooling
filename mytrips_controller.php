e17fbeb63c6e1636f0520fcd87dfbccf1da19700
Giammaria Biffi <102854627+beef-e@users.noreply.github.com>
2024-04-02 00:14:41 +0200

    Add my trips page

---
+++ mytrips_controller.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('mytrips.html', ['user' => $_SESSION]);
+
+if ($_SESSION['role'] == "d") {
+    $conn = mysqli_connect('localhost', 'pool', '', 'poolCar');
+    $query = "SELECT * FROM Viaggi WHERE id_utente_offerente = '{$_SESSION['id']}'";
+    $result = mysqli_query($conn, $query);
+    // mysqli_fetch_all restituisce un array di array associativi (quando MYSQLI_ASSOC viene passato come secondo argomento)
+
+    // $trips è un array di array associativi (ogni array associativo rappresenta un viaggio, cioè una riga della tabella Viaggi)
+    $trips = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    echo $twig->render('mytrips.html', ['user' => $_SESSION, 'trips' => $trips]);
+} else {
+    // TODO: If the user is not a driver, fetch all the bookings made by the user
+}
