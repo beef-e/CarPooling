665b8566e17e97508455c8e47d0ad8529640599f
Giammaria Biffi <102854627+beef-e@users.noreply.github.com>
2024-04-03 18:22:09 +0200

    Add Feedback handling

--- mytrips_controller.php
+++ mytrips_controller.php
@@ -39,22 +39,68 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
 }
 
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $id_viaggio = $_POST['id_viaggio'];
-    $id_utente = $_POST['id_utente'];
-    $azione = $_POST['action'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($_SESSION['ruolo'] === "d") {
+        $id_viaggio = $_POST['id_viaggio'];
+        $id_utente = $_POST['id_utente'];
+        $azione = $_POST['action'];
 
-    if ($azione == "ACC") {
-        $query = "UPDATE Prenotazioni SET stato = 'a' WHERE codice_viaggio = '{$id_viaggio}' AND id_utente = '{$id_utente}'";
-        $result = mysqli_query($conn, $query);
-        if (!$result) {
-            echo "Errore";
+        if ($azione == "ACC") {
+            $query = "UPDATE Prenotazioni SET stato = 'a' WHERE codice_viaggio = '{$id_viaggio}' AND id_utente = '{$id_utente}'";
+            $result = mysqli_query($conn, $query);
+            if (!$result) {
+                echo "Errore";
+            }
+        } else {
+            $query = "UPDATE Prenotazioni SET stato = 'r' WHERE codice_viaggio = '{$id_viaggio}' AND id_utente = '{$id_utente}'";
+            $result = mysqli_query($conn, $query);
+            if (!$result) {
+                echo "Errore";
+            }
         }
     } else {
-        $query = "UPDATE Prenotazioni SET stato = 'r' WHERE codice_viaggio = '{$id_viaggio}' AND id_utente = '{$id_utente}'";
+        $feedback = $_POST["feedback"];
+
+        //Select utenti il cui ID è in viaggi, tra i viaggi i cui id sono tra le prenotazioni dell'utente attualmente loggato
+
+        //Query fatta interamente da solo a mano sono troppo forte
+        $query = "SELECT * FROM Utenti where id IN (SELECT id_utente_offerente FROM Viaggi where id in (SELECT codice_viaggio FROM Prenotazioni WHERE id_utente='{$_SESSION['id']}'))";
+        $result = mysqli_query($conn, $query);
+        //in teoria deve ritornare sempre un solo utente
+        $driver = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+        $driver = $driver[0];
+
+        if (is_numeric($driver["media_voto"])) {
+            $media = $driver["media_voto"];
+        } else {
+            $media = 0;
+        }
+
+        $n_voti = $driver["n_voti"] + 1;
+
+        if ($n_voti === 1) {
+            $media = $feedback;
+        } else {
+            $media = ($media + $feedback) / $n_voti;
+        }
+
+
+
+        $query = "UPDATE Utenti SET media_voto = '{$media}' WHERE id = '{$driver['id']}'";
         $result = mysqli_query($conn, $query);
         if (!$result) {
-            echo "Errore";
+            echo "errore in updating media";
+        }
+        $query = "UPDATE Utenti SET n_voti='{$n_voti}' WHERE id = '{$driver['id']}'";
+        $result = mysqli_query($conn, $query);
+        if (!$result) {
+            echo "errore in updating n_voti";
+        }
+        $query = "INSERT INTO Feedback (voto, id_ut_act, id_ut_pass) VALUES ('{$feedback}', '{$_SESSION['id']}', '{$driver['id']}')";
+        $result = mysqli_query($conn, $query);
+        if (!$result) {
+            echo "errore in updating feedback table";
         }
     }
 }
